e2429a1df06753a8cb9749f7b89d954d3473ce3c
tony-toaz <wangariantony@yahoo.com>
2023-10-05 16:10:16 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*
+ * main -void
+ * Return: always 0
+ */
+int main(void)
+{
+	char *text;
+	text = "\"Programming is like building a multilingual puzzle";
+	puts(text);
+	return (0);
+}
