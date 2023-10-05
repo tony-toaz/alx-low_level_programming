a9c132ddb33fd2b13c6e01d4ab48e8d682cbcdde
tony-toaz <wangariantony@yahoo.com>
2023-10-05 16:31:03 -0700

    Hello, printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 /*
- * main -void
+ * main-  Entry point
  * Return: always 0
  */
+
 int main(void)
 {
 	char *text;
+
 	text = "\"Programming is like building a multilingual puzzle";
 	puts(text);
 	return (0);
