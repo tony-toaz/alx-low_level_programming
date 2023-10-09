48d80196d1816bc79186c7107916e0b5f6bb822b
tony-toaz <wangariantony@yahoo.com>
2023-10-08 19:00:59 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Return: 0
+ */
+
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit <= 9; digit++)
+	{
+		printf("%d", digit);
+	}
+	return (0);
+}
+
