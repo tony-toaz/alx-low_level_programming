67e193be5bc66806bdea401c5d0edae405ab7778
tony-toaz <wangariantony@yahoo.com>
2023-10-08 18:28:02 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Return: 0
+ */
+
+int main(void)
+{
+	char lower;
+	char upper;
+
+	for (lower = 'a'; lower <= 'z'; lower++)
+	{
+		putchar(lower);
+	}
+	for (upper = 'A'; upper <= 'Z'; upper++)
+	{
+		putchar(upper);
+	}
+	putchar('\n');
+	return (0);
+}
+
