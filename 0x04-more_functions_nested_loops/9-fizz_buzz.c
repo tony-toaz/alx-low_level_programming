c5ba44ded203302177b041088f61b6d72b54ee88
tony-toaz <wangariantony@yahoo.com>
2023-10-12 14:39:26 -0700

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -6,23 +6,23 @@
  */
 int main(void)
 {
-	int num;
+	int number;
 
-	for (num = 1; num <= 100; num++)
+	for (number = 1; number <= 100; number++)
 	{
-		if ((num % 3) == 0 && (num % 5) == 0)
+		if ((number % 3) == 0 && (number % 5) == 0)
 			printf("FizzBuzz");
 
-		else if ((num % 3) == 0)
+		else if ((number % 3) == 0)
 			printf("Fizz");
 
-		else if ((num % 5) == 0)
+		else if ((nuberm % 5) == 0)
 			printf("Buzz");
 
 		else
-			printf("%d", num);
+			printf("%d", number);
 
-		if (num == 100)
+		if (number == 100)
 			continue;
 		printf(" ");
 	}
