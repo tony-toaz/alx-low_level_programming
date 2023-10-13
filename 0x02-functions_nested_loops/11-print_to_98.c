f8ca3182d41125772b403fc9b257624dde14ea49
tony-toaz <wangariantony@yahoo.com>
2023-10-12 18:47:28 -0700

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,31 +3,22 @@
 
 /**
  * print_to_98 - Prints all natural numbers from n to 98,
- * @n: The number to begin counting at.
+ * @n: The number begin counted
+ * Return: void
  */
 void print_to_98(int n)
 {
-	int i;
-
-	if (n <= 98)
+	if (n >= 98)
 	{
-		for (i = n; i <= 98; i++)
-		{
-			if (i < 98)
-				printf("%d, ", i);
-			else
-				printf("%d\n", i);
-		}
-			else
-		{
-			for (i = n; i >= 98; i--)
-			{
-				if (i > 98)
-					printf("%d, ", i);
-				else
-					printf("%d\n", i);
-			}
-		}
+		while (n > 98)
+			printf("%d, ", n--);
+		printf("%d\n", n);
+	}
+
+	else
+	{
+		while (n < 98)
+			printf("%d, ", n++);
+		printf("%d\n", n);
 	}
 }
-
