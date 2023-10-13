cb5cdb604212f66bb0760dd62b7504191ca60b17
tony-toaz <wangariantony@yahoo.com>
2023-10-12 17:40:14 -0700

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet in lowercase
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		_putchar(alphabet);
+
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - Prints all natural numbers from n to 98,
+ * @n: The number to begin counting at.
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	if (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			if (i < 98)
+				printf("%d, ", i);
+			else
+				printf("%d\n", i);
+		}
+			else
+		{
+			for (i = n; i >= 98; i--)
+			{
+				if (i > 98)
+					printf("%d, ", i);
+				else
+					printf("%d\n", i);
+			}
+		}
+	}
+}
+
