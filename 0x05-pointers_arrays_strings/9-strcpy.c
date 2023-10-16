56ac841a73b14d3c243b605056d0ba5c67f2e706
tony-toaz <wangariantony@yahoo.com>
2023-10-16 03:40:33 -0700

    strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * _strcat - Concatenates @src to @dest
+ * *_strcpy - Copies a string pointed to by @src, including the
+ * terminating null byte, to a buffer pointed to by @dest
  * @dest: Pointer to the destination buffer.
  * @src: Pointer to the source string to be concatenated.
  * Return: A pointer to the destination buffer (dest).
