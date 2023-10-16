6b466584b4953f5aa21aabc30122c08d345bca9a
tony-toaz <wangariantony@yahoo.com>
2023-10-16 03:37:15 -0700

    strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,32 +1,21 @@
 #include "main.h"
 
 /**
- * _strcat - Concatenates the source string to the destination string
+ * _strcat - Concatenates @src to @dest
  * @dest: Pointer to the destination buffer.
  * @src: Pointer to the source string to be concatenated.
  * Return: A pointer to the destination buffer (dest).
  */
 char *_strcpy(char *dest, char *src)
 {
-	int dest_len = 0;
-	int src_len = 0;
-	int i, j;
+	char *original_dest = dest;
 
-	while (dest[dest_len] != '\0')
+	while (*src != '\0')
 	{
-		dest_len++;
+		*dest = *src;
+		dest++;
+		src++;
 	}
-
-	while (src[src_len] != '\0')
-	{
-		src_len++;
-	}
-
-	for (i = 0, j = dest_len; i < src_len; i++, j++)
-	{
-		dest[j] = src[i];
-	}
-
-	dest[dest_len + src_len] = '\0';
-	return (dest);
+	*dest = '\0';
+	return (original_dest);
 }
